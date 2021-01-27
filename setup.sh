be7efb63c3a0def7365543f37a1b64a78039fb9d
Javi H. Gil <javihgil@users.noreply.github.com>
2021-01-27 15:46:36 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+DOWNLOAD_PATH=$1
+UTILS=$2
+
+echo "Bash utils install tool"
+echo "  download path: $DOWNLOAD_PATH"
+echo "  utils: $UTILS"
+echo
