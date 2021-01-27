77682e41a2f0ba151e12bddb692684fc199b2d9d
Javi H. Gil <javihgil@users.noreply.github.com>
2021-01-27 16:19:42 +0100

    Update include.sh

--- include.sh
+++ include.sh
@@ -1,17 +1,20 @@
 #!/bin/bash -e
 
-echo "Bash utils include tool $UTILS_TMP_PATH"
+echo "Bash utils include tool"
 
 for TOOL in "$@"
 do
     if [ -z $UTILS_TMP_PATH ]
     then
-        echo "Include $TOOL"
+        echo " - include $TOOL"
         source <(curl -Ls https://raw.githubusercontent.com/softspring/bash-utils/main/$TOOL.sh)
     else 
         if [ ! -f "$UTILS_TMP_PATH/$TOOL.sh" ]
         then
+            echo " - downloading $TOOL"
             curl -Ls https://raw.githubusercontent.com/softspring/bash-utils/main/$TOOL.sh --output $UTILS_TMP_PATH/$TOOL.sh            
+        else
+            echo " - loading $TOOL from $UTILS_TMP_PATH"
         fi
         
         source "$UTILS_TMP_PATH/$TOOL.sh"
