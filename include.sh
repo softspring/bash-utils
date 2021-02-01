de5f75bc959f241eba5a9feb368386a1866a7ff3
Javi H. Gil <javihgil@softspring.es>
2021-02-01 12:19:38 +0100

    Update github sync

--- include.sh
+++ include.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
 
-if [ -z $UTILS_TMP_PATH ]
+if [[ -z $UTILS_TMP_PATH ]]
 then
   echo "Bash utils include tool"
 else
   echo "Bash utils include tool (cache: $UTILS_TMP_PATH)"
 fi
 
-if [ "$1" == "all" ]
+if [[ "$1" == "all" ]]
 then
     set -- "env" "files" "gcloud" "prompt" "text" "utils"
 fi
@@ -15,9 +15,9 @@ fi
 LAST_GITHUB_COMMIT=$(curl "https://api.github.com/repos/softspring/bash-utils/branches/main" 2>&1 | grep sha | head -n1 | cut -d ":" -f2- | sed 's/[" ,]//g')
 echo "Last github commit $LAST_GITHUB_COMMIT"
 
-if [ ! -z $UTILS_TMP_PATH && ! -z $LAST_GITHUB_COMMIT ]
+if [[ ! -z $UTILS_TMP_PATH && ! -z LAST_GITHUB_COMMIT ]]
 then
-  if [ ! -f "$UTILS_TMP_PATH/.version" ]
+  if [[ ! -f "$UTILS_TMP_PATH/.version" ]]
   then
     CURRENT_COMMIT=''
     echo "No current commit"
@@ -29,13 +29,13 @@ fi
 
 for TOOL in "$@"
 do
-    if [ -z $UTILS_TMP_PATH ]
+    if [[ -z $UTILS_TMP_PATH ]]
     then
         echo " - include $TOOL"
         source <(curl -Ls https://raw.githubusercontent.com/softspring/bash-utils/main/$TOOL.sh)
     else 
         mkdir -p $UTILS_TMP_PATH
-        if [ ! -f "$UTILS_TMP_PATH/$TOOL.sh" ]
+        if [[ ! -f "$UTILS_TMP_PATH/$TOOL.sh" ]]
         then
             echo " - downloading $TOOL"
             curl -Ls https://raw.githubusercontent.com/softspring/bash-utils/main/$TOOL.sh --output $UTILS_TMP_PATH/$TOOL.sh            
@@ -53,7 +53,7 @@ do
     fi    
 done
 
-if [ ! -z $UTILS_TMP_PATH && ! -z $LAST_GITHUB_COMMIT ]
+if [[ ! -z $UTILS_TMP_PATH && ! -z LAST_GITHUB_COMMIT ]]
 then
   echo $LAST_GITHUB_COMMIT > $UTILS_TMP_PATH/.version
 fi
